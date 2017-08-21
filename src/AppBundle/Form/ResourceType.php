3f104f8d9d05124a9892db992c5f2754856981c7
Uneo7 <baplancke@protonmail.com>
2017-08-21 15:30:58 +0200

    Modifications forms

--- src/AppBundle/Form/ResourceType.php
+++ src/AppBundle/Form/ResourceType.php
@@ -2,8 +2,8 @@
 
 namespace AppBundle\Form;
 
+use AppBundle\Entity\Resource;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -20,7 +20,7 @@ class ResourceType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'AppBundle\Entity\Resource'
+            'data_class' => Resource::class
         ));
     }
 
--- src/AppBundle/Form/VersionType.php
+++ src/AppBundle/Form/VersionType.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Form;
 
+use AppBundle\Entity\Version;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -20,7 +21,7 @@ class VersionType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'AppBundle\Entity\Version'
+            'data_class' => Version::class
         ));
     }
 
