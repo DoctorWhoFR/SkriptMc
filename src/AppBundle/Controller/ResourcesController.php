2832c48c9ac1f5e6afbf5ac43fe19717089f941b
DoctorWhoFR <doctorwhofr@protonmail.com>
2017-08-21 04:43:39 +0200

    Voilà ;)

--- src/AppBundle/Controller/ResourcesController.php
+++ src/AppBundle/Controller/ResourcesController.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace AppBundle\Controller;
+use AppBundle\Entity\Resource;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class ResourcesController
@@ -17,24 +19,34 @@ class ResourcesController extends Controller
      * @Route("/view", name="resources_view")
      */
 
-    public function showAction(){
+    public function showAction()
+    {
 
         $ressources = $this
             ->getDoctrine()
             ->getRepository("AppBundle:Resource")
             ->findAll();
 
-        return $this->render(':Resource:View.html.twig', array(
+        return $this->render('resource/view.html.twig', array(
             'skripts' => $ressources
         ));
     }
 
     /**
-     * @Route("/new", name="resources_view")
+     * @Route("/new", name="resources_new")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
 
-    public function newAction(){
+    public function newAction(Request $request)
+    {
+        $Resource = new Resource();
+        $form = $this->createForm('AppBundle\Form\ResourceType', $Resource);
+        $form->handleRequest($request);
 
+        return $this->render('resource/new.html.twig', array(
+            'forms' => $form->createView()
+        ));
     }
 
 
