4472da475ab61e33b2192c75135ace112721f33b
DoctorWhoFR <doctorwhofr@protonmail.com>
2017-08-21 17:34:39 +0200

    Review

---
+++ src/AppBundle/Form/ReviewType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Review;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReviewType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('Review', TextType::class, array('label' => 'Votre Review'))
+            ->add('Note', TextType::class, array('label' => 'Votre Note'));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Review::class
+        ));
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_bundle_review_type';
+    }
+}
